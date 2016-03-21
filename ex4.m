a26ce179b7b9a4ccf004bc60025c856a70122e8b
jonborg <joaomanborges@hotmail.com>
2016-03-21 19:56:02 +0000

    Added ex4

---
+++ ex4.m
@@ -0,0 +1,14 @@
+A=[0 0 1 0; 0 0 0 1; 0 566 -37 0; 0 -922 37 0];
+b=[0 0 65 -65]';
+c=[1 1 0 0];
+
+alphaK=poly([-70 -20 -10 -10]);
+alphaL=poly([-50 -50 -30 -30]);
+a=poly(A);
+
+CM=controlability(A,b);
+OM=observability(A,c);
+M=[1 0 0 0; a(2) 1 0 0; a(3) a(2) 1 0; a(4) a(3) a(2) 1];
+
+K=(alphaK(2:5)-a(2:5))*inv(M)'*inv(CM)
+L=inv(OM)*inv(M)*(alphaL(2:5)-a(2:5))'
